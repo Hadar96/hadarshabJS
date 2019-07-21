f3f994db7c7b1263b6f555896627b470b67142ce
Hadar96 <hadarshabtay96@gmail.com>
2019-07-21 15:14:39 +0300

    Update Ex1.js

--- Ex1.js
+++ Ex1.js
@@ -6,18 +6,30 @@ function printObject (obj) {
 		
 		if (typeof(val) == 'object') {
 			printObject(val);
-		} else {
+		}
+		else if (typeof(val) == 'function') { }
+		else {
 			console.log(`${key.toUpperCase()} : ${obj[key]}`);	
 		}
 	}
 }
 
-const me = {name: 'Hadar', age: 23};
-me.address = {city: 'Holon', country: 'Israel'};
+const me = {
+	name: 'Hadar', age: 23,
+	address: {city: 'Holon', country: 'Israel'},
+	getAddress() { return this.address; },
+	printMyName() { console.log(this.name); },
+	howManyRelatives() { return this.family.length; },
+	addFamilyRelative(reltv) { this.family.push(reltv); }
+};
 me.family = [
 	{name: 'Moses', member: 'Dad'},
 	{name: 'Sharon', member: 'Mom'},
 	{name: 'Or', member: 'Brother'}
 ];
+me.setName = function(newName) { this.name = newName; }
+
+var sister = {name: 'Yuval', member: 'Sister'};
+me.addFamilyRelative(sister);
 
 printObject(me);
