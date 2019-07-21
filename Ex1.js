260e4fdb74db21d298dd8e5357f7559036b31d34
Hadar96 <hadarshabtay96@gmail.com>
2019-07-21 16:44:27 +0300

    Update Ex1.js

--- Ex1.js
+++ Ex1.js
@@ -1,4 +1,4 @@
-/************************ 	Ex 1 - 21.07 	************************/
+/************************	 Ex 1 - 21.07	 ************************/
 
 function printObject (obj) {
 	for (let key in obj) {
@@ -32,7 +32,7 @@ me.setName = function(newName) { this.name = newName; }
 var sister = {name: 'Yuval', member: 'Sister'};
 me.addFamilyRelative(sister);
 
-printObject(me);
+//printObject(me);
 
 
 // Mobile phones part : 
@@ -52,10 +52,24 @@ function MobilePhone (brand, model, number, size, price) {
 	this.model = model;
 	this.number = number;
 	this.size = size;
-	this.price = price;
 	
-	this.getNumber = function () { return this.number; }
-	this.setNumber = function (number) { this.number = number; }
+	let _price;
+	
+	this.getPrice = function () { return _price; }
+	this.setPrice = function (newPrice) {
+		if (newPrice <= 0) {
+			console.error(`${newPrice} is negative: Price of phone must be positive!`);
+		}
+		else {
+			if (isNaN(_price) || newPrice >= _price) {
+				_price = newPrice;
+			} else {
+				console.error(`${newPrice} is smaller then ${_price}: New price of phone must be bigger then the old price!`);
+			}
+		}
+	}
+	
+	this.setPrice(price);
 }
 
 const phone1 = CreateMobilePhone('LG', 'G', 5, 5.64, 2100);
