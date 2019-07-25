714243e62ba45d800647d7b5b5138d5a6ba7723c
Hadar96 <hadarshabtay96@gmail.com>
2019-07-25 10:24:22 +0300

    Add files via upload

--- 23_07/cellphoneCtrl.js
+++ 23_07/cellphoneCtrl.js
@@ -1,9 +1,9 @@
 app.controller("cellphoneCtrl", function ($scope) {
     $scope.phones = [
-        new Phone("IPhone X", "white", 3500),
+        new Phone("iPhone X", "white", 3500),
         new Phone("Xiaomi", "lightgreen", 1000),
         new Phone("Samsung Galaxy 11+", "silver", 5500),
-        new Phone("LG V60", "lightblue", 7500)
+        new Phone("LG v60", "lightblue", 7500)
     ];
 
     $scope.remove = function (phone) {
@@ -15,7 +15,7 @@ app.controller("cellphoneCtrl", function ($scope) {
         if (model && model.trim() !== '' &&
             color && color.trim() !== '' &&
             price && price !== '') {
-            $scope.phones.push(new Phone(model, color, price));
+            $scope.phones.push(new Phone(model, $scope.getColorNameByHex(color), price));
         }
     }
 
@@ -24,7 +24,7 @@ app.controller("cellphoneCtrl", function ($scope) {
             $scope.phones[id].model = model;
 
         if (color && color.trim() !== '')
-            $scope.phones[id].color = color;
+            $scope.phones[id].color = $scope.getColorNameByHex(color);
 
         if (price && price !== '' && price > 0)
             $scope.phones[id].price = price;
@@ -35,7 +35,7 @@ app.controller("cellphoneCtrl", function ($scope) {
 
     $scope.turnEditModeOn = function (id) {
         $scope.editModel = $scope.phones[id].model;
-        $scope.editColor = $scope.phones[id].color;
+        $scope.editColor = $scope.getColorHexByName($scope.phones[id].color);
         $scope.editPrice = $scope.phones[id].price;
 
         $scope.editedRow = id;
@@ -49,6 +49,33 @@ app.controller("cellphoneCtrl", function ($scope) {
             $scope.orderParam = param;
         }
     }
+
+    $scope.getColorNameByHex = function (hex) {
+        if (!hex) return;
+
+        hex = hex.replace('#', '');
+        let idx = colorHexes.indexOf(hex);
+
+        if (idx === -1) {
+            return '#' + hex;
+        } else {
+            return colorNames[idx];
+        }
+    }
+
+    $scope.getColorHexByName = function (name) {
+        let idx = colorNames.indexOf(name.toLowerCase());
+
+        if (idx === -1) {
+            return name;
+        } else {
+            return '#' + colorHexes[idx];
+        }
+    }
+
+    let colorNames = ['aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige', 'bisque', 'black', 'blanchedalmond', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgrey', 'darkgreen', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkslategrey', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dimgrey', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'grey', 'green', 'greenyellow', 'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgrey', 'lightgreen', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightslategrey', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen', 'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orange', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple', 'rebeccapurple', 'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'slategrey', 'snow', 'springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'white', 'whitesmoke', 'yellow', 'yellowgreen'];
+
+    let colorHexes = ['f0f8ff', 'faebd7', '00ffff', '7fffd4', 'f0ffff', 'f5f5dc', 'ffe4c4', '000000', 'ffebcd', '0000ff', '8a2be2', 'a52a2a', 'deb887', '5f9ea0', '7fff00', 'd2691e', 'ff7f50', '6495ed', 'fff8dc', 'dc143c', '00ffff', '00008b', '008b8b', 'b8860b', 'a9a9a9', 'a9a9a9', '006400', 'bdb76b', '8b008b', '556b2f', 'ff8c00', '9932cc', '8b0000', 'e9967a', '8fbc8f', '483d8b', '2f4f4f', '2f4f4f', '00ced1', '9400d3', 'ff1493', '00bfff', '696969', '696969', '1e90ff', 'b22222', 'fffaf0', '228b22', 'ff00ff', 'dcdcdc', 'f8f8ff', 'ffd700', 'daa520', '808080', '808080', '008000', 'adff2f', 'f0fff0', 'ff69b4', 'cd5c5c', '4b0082', 'fffff0', 'f0e68c', 'e6e6fa', 'fff0f5', '7cfc00', 'fffacd', 'add8e6', 'f08080', 'e0ffff', 'fafad2', 'd3d3d3', 'd3d3d3', '90ee90', 'ffb6c1', 'ffa07a', '20b2aa', '87cefa', '778899', '778899', 'b0c4de', 'ffffe0', '00ff00', '32cd32', 'faf0e6', 'ff00ff', '800000', '66cdaa', '0000cd', 'ba55d3', '9370db', '3cb371', '7b68ee', '00fa9a', '48d1cc', 'c71585', '191970', 'f5fffa', 'ffe4e1', 'ffe4b5', 'ffdead', '000080', 'fdf5e6', '808000', '6b8e23', 'ffa500', 'ff4500', 'da70d6', 'eee8aa', '98fb98', 'afeeee', 'db7093', 'ffefd5', 'ffdab9', 'cd853f', 'ffc0cb', 'dda0dd', 'b0e0e6', '800080', '663399', 'ff0000', 'bc8f8f', '4169e1', '8b4513', 'fa8072', 'f4a460', '2e8b57', 'fff5ee', 'a0522d', 'c0c0c0', '87ceeb', '6a5acd', '708090', '708090', 'fffafa', '00ff7f', '4682b4', 'd2b48c', '008080', 'd8bfd8', 'ff6347', '40e0d0', 'ee82ee', 'f5deb3', 'ffffff', 'f5f5f5', 'ffff00', '9acd32'];
 });
 
 class Phone {
